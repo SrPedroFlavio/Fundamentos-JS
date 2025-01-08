3027b0a86f635cf6d9b11068f7e82277703909f1
SrPedroFlavio <pedro05062006@gmail.com>
2025-01-08 15:55:00 -0300

    Array e funções

---
+++ funcaoArray.js
@@ -0,0 +1,75 @@
+// let numeros = [1, 7, 3, 9]
+
+// // Map() Cira novo array transformando os elementos
+// let numerosDobro = numeros.map(num => num * 2)
+
+// console.log(numerosDobro);
+
+// // Find() Retorna o primeiro elemento que sastifaz a condição
+// let primeiro = numeros.find(n => n == 2)
+// console.log(primeiro);
+
+// let nomezitos = ["André", "Angelica", "Natalia"]
+// let primeironomeT = nomezitos.find(nomes => nomes.includes("t"))
+
+// console.log(primeironomeT);
+
+// // Filter() Cira um novo array que satifazem a condição
+// let maioresQue3 = numeros.filter(n => n > 1)
+// console.log(maioresQue3);
+
+// // Includes() Se algo está incluido na array
+// let existe5 = numeros.includes(5)
+// console.log(existe5);
+
+// // Some() Verificar um elemento satifaz a condição
+// let par = numeros.some(n => n % 2 === 0)
+// console.log(par);
+
+// // ForEach() Igual o for kek
+// let nomes2 = nomezitos.forEach((nomes) => console.log(nomes))
+
+
+//////////////////////////////////////////////////
+let number = [1, 2, 3, 4, 5]
+
+let dobro = number.map(c => c * 2)
+console.log(dobro);
+
+
+let number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let pares = number2.filter(n1 => n1 % 2 == 0)
+console.log(pares);
+
+
+let decimos = [10, 25, 47, 58, 62, 79]
+let maiorQ10 = decimos.find(n2 => n2 > 50)
+console.log(maiorQ10);
+
+
+let negativos = [10, -5, 7, 0, -3]
+let umNegativo = negativos.some(n => n < 0)
+console.log(umNegativo);
+
+
+
+let lele = [1, 2, 3, 4, 5]
+let maiorq2 = lele.every(n => n > 2)
+console.log(maiorq2);
+
+
+let entrada = [10, 20, 30, 40, 50]
+let entrada2 = entrada.forEach((entrada, index) => console.log(`Número, ${entrada}, está na posição ${index}`));
+
+
+
+let caju = [1, 3, 5, 7, 9, 10]
+let cajao = caju.includes(7)
+console.log(cajao);
+
+
+
+
+
+
+
---
+++ funcoes.js
@@ -0,0 +1,96 @@
+// function funcaoBomDia(){
+//     console.log("Boa Dia");
+// }
+
+// funcaoBomDia ()
+
+
+
+// function funcaoBemVindo(nome){
+//     console.log("Bem vindo", nome);
+// }
+
+// funcaoBemVindo("Pedro")
+// funcaoBemVindo("Sil")
+
+
+// function soma(numero1, numero2){
+//     console.log(numero1 + numero2, numero1 * numero2, numero1 / numero2);
+// }
+
+// soma(2, 4)
+
+// function maioridade(idade){
+//     if(idade >= 100){
+//         console.log("Ta ancião ja");        //cuidado para 1 n anular outro
+    
+//     } else if(idade >= 18){
+//         console.log("maior de idade");
+//     }  
+//     else {
+//         console.log("menor de idade");
+//     }
+// }
+
+// maioridade(180)
+
+
+// function divisao(numero0, numero00){
+//     return numero0 / numero00
+
+// }
+// let resultado = divisao(17, 7) 
+// console.log(resultado);
+
+
+
+// function dobro(n1){
+//     return n1 * 2
+// }
+// let multp = dobro(10)
+// console.log(multp);
+
+
+// let sub = function(nn2, nn1){
+//     return nn2 - nn1
+// }
+
+// console.log(sub(10, 0.5));
+
+
+
+function calculaIdade(idade){
+    return 2025 - idade
+}
+
+console.log(calculaIdade(21));
+
+
+function calcularMedia(nt1, nt2, nt3){
+    return (nt1 + nt2 + nt3) / 3
+}
+
+console.log(calcularMedia(20, 10 ,0 ));
+
+
+
+
+
+function verificarParImpar(numb){
+    if(numb % 2 == 0){
+        console.log("par");
+
+    }else{
+        console.log("impar");
+    }
+}
+
+verificarParImpar(7)
+
+
+
+let doblo = (numero) => numero * 2
+
+
+
+
